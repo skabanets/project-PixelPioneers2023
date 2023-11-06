a8f3d719f2b4ba641a9e0898c876c4dcf771199e
Sergey Kabanets <skabanets97@gmail.com>
2023-11-06 17:08:44 +0200

    fixed form order issues

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@
   const closeMenuBtn = document.querySelector('.js-close-menu');
   const backdrop = document.querySelector('.backdrop');
   const heroBtn = document.querySelector('.hero-btn');
+  const orderForm = document.querySelector('.your-order-form');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -31,6 +32,10 @@
     if (e.target.hasAttribute('data-sidebar')) {
       toggleMenu();
     }
+  });
+
+  orderForm.addEventListener('submit', e => {
+    e.preventDefault();
   });
 
   // Close the mobile menu on wider screens if the device orientation changes
